9e0c4093c178fed2808d6fef049e2e20198f0f68
Miguel Ángel Barquilla <46623652+Mibarfu@users.noreply.github.com>
2022-10-18 00:46:30 +0200

    Implement initial Info class

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,10 +1,12 @@
-import React from "react";
 import "./App.css";
+import "../Info/Info";
+import Info from "../Info/Info";
 
 const App = (): JSX.Element => {
   return (
     <header className="main-header">
       <h1 className="main-title">The pointing gentlemen</h1>
+      <Info counts={3} />
     </header>
   );
 };
