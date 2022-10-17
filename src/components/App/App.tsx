987938caf80edd26783f0f923872c441d63b805f
Miguel Ángel Barquilla <46623652+Mibarfu@users.noreply.github.com>
2022-10-18 01:09:03 +0200

    Include length of the gentleman of the list in App component

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,12 +1,15 @@
 import "./App.css";
 import "../Info/Info";
 import Info from "../Info/Info";
+import dataGentleman from "../../data/dataGentleman";
 
 const App = (): JSX.Element => {
+  const lenghtGentleman = dataGentleman.length;
+
   return (
     <header className="main-header">
       <h1 className="main-title">The pointing gentlemen</h1>
-      <Info counts={3} />
+      <Info counts={lenghtGentleman} />
     </header>
   );
 };
