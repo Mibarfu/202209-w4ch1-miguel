a2000fa49e9bfb68bc749fa4e46b069e07ae60fd
Miguel Ángel Barquilla <46623652+Mibarfu@users.noreply.github.com>
2022-10-18 00:04:11 +0200

    Add header h1

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,11 @@ import React from "react";
 import "./App.css";
 
 const App = (): JSX.Element => {
-  return <div></div>;
+  return (
+    <header className="main-header">
+      <h1 className="main-title">The pointing gentlemen</h1>
+    </header>
+  );
 };
 
 export default App;
